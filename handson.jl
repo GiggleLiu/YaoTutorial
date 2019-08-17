9d7942068692c1982ddb642c2ca301566ce9a7a4
GiggleLiu <cacate0129@gmail.com>
2019-08-17 16:23:17 +0800

    update

--- handson.jl
+++ handson.jl
@@ -1,27 +1,57 @@
 using Yao, QuAlgorithmZoo
 
-qc = QFTCircuit(4)
+# Quantum Block Intermediate Representation
+qft = QFTCircuit(4)
 
-mat(qc)
+@edit QFTCircuit(4)
 
-ishermitian(qc)
-isunitary(qc)
-isreflexive(qc)
+# get matrix
+mat(qft)
 
+# analyze properties
+ishermitian(qft)
+isunitary(qft)
+isreflexive(qft)
+
+# dagger
+iqft = qft'
+
+# arithmatics
+MyHGate = Rz(-π/2)*Rx(-π/2)*Rz(-π/2)
+mat(phase(-π/2)*MyHGate) ≈ mat(H)
+
+# tuning the structure
+function replace_hadamard(tree)
+    tree isa HGate && return deepcopy(MyHGate)
+    return chsubblocks(tree, replace_hadamard.(subblocks(tree)))
+end
+
+myqft = replace_hadamard(qft)
+
+# apply an algorithm
+reg = product_state(bit"0110")
+out = copy(reg) |> qft
+copy(out) |> iqft ≈ reg
+
+measure(out; nshots=100)
+
+# the performance
 reg = rand_state(20)
-reg |> concentrate(20, qc, (4,1,6,5))
+reg |> concentrate(20, qft, (4,5,6,7))
 
 using BenchmarkTools
-@benchmark $reg |> $(concentrate(20, qc, (4,1,6,5))) seconds=1
+@benchmark $reg |> $(concentrate(20, qft, (4,1,6,5))) seconds=1
 
+# show to use GPU power
 using CuYao, CuArrays
 creg = reg |> cu
-@benchmark CuArrays.@sync $creg |> $(concentrate(20, qc, (4,1,6,5)))
+@benchmark CuArrays.@sync $creg |> $(concentrate(20, qft, (4,1,6,5)))
 
 nbit = 16
 dc = random_diff_circuit(nbit)
 dc = dc |> autodiff(:BP)
 
+# parameter management
 dispatch!(dc, :random)
 
 out = zero_state(nbit) |> dc
@@ -30,11 +60,9 @@ sx(i) = put(nbit, i=>X)
 sy(i) = put(nbit, i=>Y)
 sz(i) = put(nbit, i=>Z)
 
-#hami = sum([sx(i)*sx(i+1)+sy(i)*sy(i+1)+sz(i)*sz(i+1) for i=1:nbit-1])
-hami = sum([sx(i)*sx(i+1)+sy(i)*sy(i+1)+sz(i)*sz(i+1) for i=1:1])
+hami = sum([sx(i)*sx(i+1)+sy(i)*sy(i+1)+sz(i)*sz(i+1) for i=1:nbit-1])
 
 ∇out = copy(out) |> hami
-#@benchmark backward!((copy(out), ∇out), dc)
 backward!((copy(out), ∇out), dc)
 grad = gradient(dc)
 
