185f25a8eeb10139434c376324d4c6ef1748cc99
GiggleLiu <cacate0129@gmail.com>
2023-10-07 06:15:19 +0000

    update cuda

--- clips/README.md
+++ clips/README.md
@@ -1,3 +1,8 @@
 # Yao Live Coding
 
 Powered by [AsciinemaGenerator](https://github.com/GiggleLiu/AsciinemaGenerator.jl)
+
+```bash
+bash cast.sh yao-v0.8
+bash cast.sh yao-v0.8-cuda
+```
--- clips/yao-v0.8-cuda.jl
+++ clips/yao-v0.8-cuda.jl
@@ -1,27 +1,39 @@
 #== QFT simulation on GPU ==#
-# show to use GPU power
 using CuYao
 
 nqubits = 20
 
-qft = EasyBuild.qft_circuit(nqubits)
+#s output_delay=2
+qft = EasyBuild.qft_circuit(nqubits);
 
+#s output_delay=0.5
 # initialize a quantum state
 reg = zero_state(nqubits)
 
-# upload it to GPU
+# upload the quantum state to GPU
 creg = reg |> cu
 
+#+ 2
 # it is equivalent to
 CuYao.cuzero_state(nqubits)
 
-# excute the Fourier transformation on qubits (4,6,7)
-creg |> qft
+# execute the Fourier transformation circuit
+apply(creg, qft)
+#+ 2
 
 # measure on the output
-measure(creg; nshots=10)
+measure(creg; nshots=5)
+#+ 2
 
+# compute expectation values X₂X₈
+expect(kron(nqubits , 2=>X, 8=>X), creg)
+
+#+ 2
 using BenchmarkTools
+
+#s output_delay=3
+# the CPU version
 @btime apply($reg, $qft)
 
+# the GPU version
 @btime CuYao.CUDA.@sync apply($creg, $qft)
